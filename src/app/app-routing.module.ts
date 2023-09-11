7fe2f289005b2de8dafc774e20ea08905dbc1cb7
yossirivkin <yyr736@gmail.com>
2023-09-11 14:36:26 +0300

    home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultListComponent } from './components/result-list/result-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'url-results/:urlParam', component: ResultListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeedUrlsListComponent } from './components/seed-urls-list/seed-urls-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NewScreapingComponent } from './components/new-scraping/new-scraping.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ResultListComponent } from './components/result-list/result-list.component';
+import { HomeComponent } from './home/home.component';
+import { UrlHostDirective } from './directives/url-host.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeedUrlsListComponent,
+    NewScreapingComponent,
+    ResultListComponent,
+    HomeComponent,
+    UrlHostDirective
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
